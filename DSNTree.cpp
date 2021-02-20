98c5f97a8344ae6915d21e7b4b77c3502591127f
krisk78 <c.couaillet@gmail.com>
2021-02-20 03:09:16 +0100

    Release 1.02

--- DSNTree.cpp
+++ DSNTree.cpp
@@ -3,7 +3,21 @@
 
 #include <iostream>
 
-int main(int argc, char argv[])
+#include "DSNTreeApp.hpp"
+
+int main(int argc, char* argv[])
 {
-    std::cout << "Hello World!\n";
+    DSNTreeApp app(false);
+
+    auto ret = app.Arguments(argc, argv);
+	if (ret.length() != 0 && ret != "?")
+		return -1;
+	if (ret == "?")		// help was requested and usage has been printed
+		return 0;
+	try {
+		app.Run(); }
+	catch (const std::exception& e) {
+		std::cout << "Execution error " << e.what() << std::endl;
+		return -1; }
+	return 0;
 }
